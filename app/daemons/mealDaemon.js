571ffe7053a7341ad021f9c156c52b924e3a1957
Luka Ruklić <ruklic.luka@gmail.com>
2017-11-26 00:23:51 +0100

    Added scheduler, HTML parse and new restaurant type

--- app/daemons/mealDaemon.js
+++ app/daemons/mealDaemon.js
@@ -2,25 +2,34 @@ var DataController = require('../db/data');
 
 var https = require('https');
 var graph = require('fbgraph');
-//var moment = require('node-moment');
+var ontime = require('ontime');
+var cheerio = require('cheerio');
+var request = require('request');
+var moment = require('moment');
 
 var MealMenu = require('../models/mealmenu');
 
 module.exports = function () {
 
-    //fetchBTW();
+    ontime({
+        cycle: '08:30:00'
+    }, function (ot) {
+        console.log('Fetching BTW menu for ' + new Date());
+        //fetchBTW();
+        ot.done();
+        return;
+    });
 
+    fetchVujca();
+    
 };
 
 function fetchBTW() {
 
-    var appId = ""; // enter app ID
-    var appSecret = ""; // enter app secret
-
-
+    var appId = "1669092963136035";
+    var appSecret = "c6a1560e72195a8364f6c0317384483f";
 
     graph.get("BTWrestaurant/posts?access_token=" + appId + "|" + appSecret, function(err, res) {
-        console.log(res); 
         var mealMenu = new MealMenu();
         mealMenu.date = new Date(res.data[0].created_time);
         mealMenu.restaurant = "btw";
@@ -34,4 +43,42 @@ function fetchBTW() {
             console.log('mealMenu saved');       
         });
     });
+}
+
+function fetchVujca() {
+
+    request("http://jambresic.com/gableci/", function(error, response, html){        
+        if(!error){
+            var $ = cheerio.load(html);
+        
+            $("img.fl-photo-img").each(function() {
+                var foodImage = $(this)[0];
+
+                var day = foodImage.attribs.alt;
+                if (dayIndex(day) >= 0) {
+                    var mealMenu = new MealMenu();
+                    mealMenu.date = getDayOffset(day.toUpperCase()).toDate();
+                    mealMenu.restaurant = "vujca";
+                    mealMenu.meals = foodImage.attribs.src;
+                    
+                    mealMenu.save(function(err) {
+                        if (err){
+                            console.log('Error in Saving mealMenu: '+err);  
+                            throw err;  
+                        }
+                        console.log('mealMenu saved');       
+                    });
+                }           
+            });
+        }
+    })
+}
+
+function dayIndex(day) {
+    return ['PONEDJELJAK', 'UTORAK', 'SRIJEDA', 'CETVRTAK', 'PETAK', 'SUBOTA'].indexOf(day.toUpperCase());
+}
+
+function getDayOffset(day) {
+    return moment(new Date()).add(dayIndex(day), 'days');
+
 }
