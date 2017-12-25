f58767187b747afb001d49ffe5e7a2623474bd45
Luka Ruklić <ruklic.luka@gmail.com>
2017-12-25 17:12:36 +0100

    Added mongo session store to DB

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var flash = require('connect-flash');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
 
 var app = express();
@@ -19,7 +20,10 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
 // Initialize session
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
+app.use(session({ 
+    secret: 'ilovescotchscotchyscotchscotch',
+    store: new MongoStore({ url : 'mongodb://127.0.0.1:27017'})
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
